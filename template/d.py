37e23ec5c221e8db3ac7baa36561df423bbe09ce
kromash <k.romaszko@sotrender.com>
2021-12-24 16:10:16 +0100

    Fix task template

--- template/d.py
+++ template/d.py
@@ -1,2 +1,23 @@
+def read_input(filename):
+    input = []
+    with open(filename) as f:
+        for line in f.readlines():
+            input.append(line.strip())
+
+    return input
 
 
+def part_one(input):
+    result = 0
+    print(f'part one answer: {result}')
+
+
+def part_two(input):
+    result = 0
+    print(f'part two answer: {result}')
+
+
+if __name__ == '__main__':
+    input = read_input('d_ex.txt')
+    part_one(input)
+    # part_two(input)
